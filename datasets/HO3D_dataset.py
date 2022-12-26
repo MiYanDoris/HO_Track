fix rotation bug in optimization

datasets/HO3D_dataset.py
@@ -141,6 +141,11 @@ def generate_HO3D_data(mano_layer_right, root_dir, seq, fID, num_points, obj_per
     hand_kp = hand_kp[reorder]
     world_trans = hand_kp[0]
 
+    # print('#'*50)
+    # print(seq, fID)
+    # print(obj_pose)
+    # print(hand_pcld.shape, obj_pcld.shape)
+    # print(hand_pcld[1000], obj_pcld[1000])
     # remove outliers
     obj_dis = np.linalg.norm(obj_pcld - obj_pose['translation'].transpose(-1,-2), axis=-1)
     foreground = np.where(obj_dis < 0.25)
@@ -149,7 +154,7 @@ def generate_HO3D_data(mano_layer_right, root_dir, seq, fID, num_points, obj_per
     hand_dis = np.linalg.norm(hand_pcld - hand_kp[9], axis=-1)
     foreground = np.where(hand_dis < 0.15)
     hand_pcld = hand_pcld[foreground]
-
+    
     # point cloud downsample
     sample_idx = farthest_point_sample(hand_pcld, num_points, device)
     hand_pcld = hand_pcld[sample_idx]

network/models/optimization_hand.py
@@ -2,27 +2,13 @@ import numpy as np
 import torch
 import pickle
 from network.models.hand_utils import mano_axisang2quat, mano_quat2axisang
-from pose_utils.rotations import matrix_to_unit_quaternion, unit_quaternion_to_matrix
+from pose_utils.rotations import matrix_to_unit_quaternion,compute_rotation_matrix_from_ortho6d, unit_quaternion_to_matrix
 from third_party.DeepSDF.deep_sdf_decoder import Decoder
 from third_party.mano.our_mano import OurManoLayer
 import cv2
 from optimization_obj import CatCS2InsCS, InsCS2CatCS
 from os.path import join as pjoin
 
-
-def world2mask(xyz, device):
-    '''
-        xyz: B, N, 3
-    '''
-    B = xyz.shape[0]
-    mask = torch.zeros((B, h, w), device=device, dtype=torch.bool)
-    x = (xyz[..., 0] / xyz[..., 2] * fx + w // 2).long()
-    y = (xyz[..., 1] / xyz[..., 2] * fy + h // 2).long()
-    x = torch.clamp(x, 0, w-1)
-    y = torch.clamp(y, 0, h-1)
-    help = torch.arange(x.shape[0])[:,None].repeat(1, x.shape[1])
-    mask[help, y, x] = True
-    return mask.bool()
 
 def world2point2D(xyz, fx, fy, cx, cy):
     '''
@@ -244,7 +230,7 @@ class gf_optimize_hand_pose():
         pred_2D = world2point2D(hand, self.proj['fx'][0], self.proj['fy'][0], self.proj['cx'][0], self.proj['cy'][0])   #[B, N, 2]
         index1 = torch.clamp(pred_2D[...,0].long(), 0, self.h-1)
         index2 = torch.clamp(pred_2D[...,1].long(), 0, self.w-1)
-        silhouette_loss = self.gt_mask[index1, index2]
+        silhouette_loss = self.gt_background_mask[index1, index2]
         silhouette_loss = silhouette_loss.sum(dim=-1) / pred_2D.shape[1]
         return silhouette_loss
 
@@ -323,23 +309,23 @@ class gf_optimize_hand_pose():
         
         # read silhouette 
         if self.data_config == 'data_info_HO3D.yml':
-            silhouette_pth = pjoin(self.root_dir, 'pred_mask/ensemble/%s/%s.png' % (file_name.split('/')[0], file_name.split('/')[1]))
-            mask = (cv2.imread(silhouette_pth) / 70)[:240] # 0:obj,1:hand,2back
-            mask = cv2.resize(mask, (640, 480), interpolation=cv2.INTER_NEAREST)[:, :, 0]
-            self.gt_mask = (mask == 2)
+            silhouette_pth = pjoin(self.root_dir, 'train/%s/seg/%s.png' % (file_name.split('/')[0], file_name.split('/')[1]))
+            mask = cv2.imread(silhouette_pth) 
+            mask = cv2.resize(mask, (640, 480), interpolation=cv2.INTER_NEAREST)
+            self.gt_background_mask =  mask.sum(axis=-1) == 0
         elif self.data_config == 'data_info_SimGrasp.yml':
             silhouette_pth = pjoin(self.root_dir, 'img/%s/seq/%s/mask.png' % (category, file_name))
             maskimg = cv2.imread(silhouette_pth)
-            self.gt_mask = maskimg.sum(axis=-1) == 0
+            self.gt_background_mask = maskimg.sum(axis=-1) == 0
         elif self.data_config == 'data_info_DexYCB.yml':
             silhouette_pth = pjoin(self.root_dir, 'tarfolder/%s/%s/%s/labels_%s.npz' % (file_name.split('+')[0],file_name.split('+')[1],file_name.split('+')[2],file_name.split('+')[3]))
             color_pth = silhouette_pth.replace('labels', 'color').replace('npz', 'jpg')
             rgbimg = cv2.imread(color_pth)
             maskimg = np.load(silhouette_pth)['seg']
-            self.gt_mask = maskimg==0
-            self.rgbimg = rgbimg * self.gt_mask[:,:,None]
+            self.gt_background_mask = maskimg==0
+            self.rgbimg = rgbimg * self.gt_background_mask[:,:,None]
 
-        self.gt_mask = torch.tensor(self.gt_mask).to(self.device)
+        self.gt_background_mask = torch.tensor(self.gt_background_mask).to(self.device)
 
     def optimize(self, init_mano, init_hand_pose, init_kp, last_frame_kp, vis_mask, init_obj_pose, category, file_name, hand_shape, projection):
         # initialize 
@@ -378,6 +364,9 @@ class gf_optimize_hand_pose():
                 mean_transform = (sample * weight.unsqueeze(1)).sum(dim=0, keepdim=True) / weight_sum    #[1, 7]
                 mean_transform[:, :4] /= mean_transform[:,:4].norm()
                 self.curr_r = torch.matmul(self.curr_r, unit_quaternion_to_matrix(mean_transform[:, :4])) 
+                # NOTE: It is necessary to project rotation back to the SO3 since accumulate product may cause numerial error 
+                self.curr_r  = compute_rotation_matrix_from_ortho6d(self.curr_r.reshape(-1, 9)[:,:6]).transpose(-1,-2)
+                
                 self.curr_t = self.curr_t + mean_transform[:, 4:7, None]
                 self.curr_theta = self.curr_theta + self.mano_layer_right.pca_comps2pose(self.ncomps, mean_transform[:, 7:])*self.theta_scale
             else:

network/models/optimization_obj.py
@@ -1,6 +1,6 @@
 import numpy as np
 import torch
-from pose_utils.rotations import unit_quaternion_to_matrix
+from pose_utils.rotations import unit_quaternion_to_matrix, compute_rotation_matrix_from_ortho6d
 import os
 import cv2
 import open3d as o3d
@@ -277,8 +277,10 @@ class gf_optimize_obj():
             #update R, T
             if success_flag:
                 mean_transform = (sample * weight.unsqueeze(1)).sum(dim=0, keepdim=True) / weight_sum    #[1, 7]
-                mean_transform[:, :4] /= (mean_transform[:,:4].norm() + 1e-5)
+                mean_transform[:, :4] /= (mean_transform[:,:4].norm() + 1e-8)
                 rotation = torch.matmul(rotation, unit_quaternion_to_matrix(mean_transform[:, :4])) 
+                # NOTE: It is necessary to project rotation back to the SO3 since accumulate product may cause numerial error 
+                rotation = compute_rotation_matrix_from_ortho6d(rotation.reshape(-1, 9)[:,:6]).transpose(-1,-2)
                 translation = translation + mean_transform[:, 4:, None]
             else:
                 mean_transform = torch.zeros((1,7), device=self.device)

network/models/track_network.py
@@ -187,7 +187,7 @@ class HandTrackModel(nn.Module):
                 # optimize MANO pose code and hand global RT to improve HO interaction
                 if self.use_optimization:
                     obj_pose = data['pred_obj_pose'] if self.use_pred_obj_pose else data['gt_obj_pose']
-                    optimized_kp, optimized_mano, optimized_rot_mat, optimized_t, ret_dict['max_penetr'] = self.optimizer.optimize(ret_dict['MANO_theta'], ret_dict['global_pose'],
+                    optimized_kp, optimized_mano, optimized_rot_mat, optimized_t = self.optimizer.optimize(ret_dict['MANO_theta'], ret_dict['global_pose'],
                                                      ret_dict['baseline_pred_kp'], last_frame_kp, ret_dict['pred_kp_vis_mask'], obj_pose, data['category'][0],
                                                      data['file_name'][0], shape_code, data['projection'])
                     ret_dict['pred_kp'] = optimized_kp
@@ -221,8 +221,6 @@ class HandTrackModel(nn.Module):
 
         for i, data in enumerate(input):
             loss_dict, _ = self.handnet.compute_loss(data, ret_dict_lst[i], flag_dict)
-            if self.use_optimization and self.IKnet is not None:
-                loss_dict['max_penetration'] = ret_dict_lst[i]['max_penetr'] if ret_dict_lst[i]['max_penetr'] > 0  else 0
             add_dict(total_loss, loss_dict)
             if i == 0:
                 init_loss = deepcopy(loss_dict)
@@ -416,7 +414,6 @@ class ObjTrackModel_Optimization(nn.Module):
 
                 eval_pred_obj_pose = {'rotation': torch.matmul(ret_dict_lst[i]['rotation'], R.transpose(-1,-2))}
                 eval_pred_obj_pose['translation'] = ret_dict_lst[i]['translation'] - torch.matmul(eval_pred_obj_pose['rotation'], T)
-
             else:
                 eval_gt_obj_pose = data['gt_obj_pose']
                 eval_pred_obj_pose = ret_dict_lst[i]
@@ -425,9 +422,6 @@ class ObjTrackModel_Optimization(nn.Module):
             transformed_gt_mesh = torch.matmul(gt_mesh.clone(), data['gt_obj_pose']['rotation'].squeeze().transpose(-1,-2)) + data['gt_obj_pose']['translation'].squeeze()
             transformed_pred_mesh = torch.matmul(pred_mesh.clone(), ret_dict_lst[i]['rotation'].squeeze().transpose(-1,-2)) + ret_dict_lst[i]['translation'].squeeze()
             loss_dict['pred_obj_chamfer(mm)'] = compute_chamfer(transformed_gt_mesh, transformed_pred_mesh) * 1000
-            # print(i, loss_dict['pred_obj_chamfer(mm)'], loss_dict['rdiff_0'], loss_dict['tdiff_0'])
-            # if loss_dict['pred_obj_chamfer(mm)'] / 1000 < 0.1*diag:
-                # ADDcount += 1/len(input)
             add_dict(total_loss, loss_dict)
             if save_flag:
                 merge_dict(save_dict, {'gt_hand_poses':data['gt_hand_pose'],